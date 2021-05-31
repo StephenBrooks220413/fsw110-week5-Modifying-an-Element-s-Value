b49dd6148891f06b5ee6768118314a6d14b9f809
stephenbrooks40 <stephenbrooks714@gmail.com>
2021-05-31 10:05:03 -0700

    files

---
+++ index.js
@@ -0,0 +1,7 @@
+function btn(clear) {
+    var x = document.getElementById("btn");
+    x.onclick = alert('Message Sent');
+    if (alert) {
+        document.getElementsByTagName("input").clear;
+    }
+}
